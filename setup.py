cc3194732bd12088da775ea7faddbe0e523a6438
Thomas Burke <tburke@wapolabs.com>
2012-05-31 16:36:49 -0400

    just a framework.

---
+++ setup.py
@@ -0,0 +1,17 @@
+##############################################################################
+#
+# Copyright (c) 2011, 2012 Wapolabs
+# All Rights Reserved.
+#
+##############################################################################
+__version__ = '0.1.0'
+
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+
