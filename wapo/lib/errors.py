c972cbd2a517d3fc25f186627f2ab6c2f57fc71e
Sean P. McBride <sean@wapolabs.com>
2012-06-04 18:08:19 -0400

    handle HTTPErrors via RestError.__call__()

--- wapo/lib/errors.py
+++ wapo/lib/errors.py
@@ -1,62 +1,62 @@
-import logging
+from json import dumps
 from pyramid.httpexceptions import HTTPError
-from pyramid.view import view_config
-
-log = logging.getLogger(__name__)
+from pyramid.request import Response
 
 class RestError(HTTPError):
     """
     Error Response
+    Implement with:
+        # TODO doublecheck import
+        from errors import RestError
+        config.add_view(RestError, context=HTTPError)
     """
     
-    status_codes = {
-        400:'Bad Request',
-        401:'Unauthorized',
-        402:'Payment Required',
-        403:'Forbidden',
-        404:'Not Found',
-        405:'Method Not Allowed',
-        406:'Not Acceptable',
-        407:'Proxy Authentication Required',
-        408:'Request Timeout',
-        409:'Conflict',
-        410:'Gone',
-        411:'Length Required',
-        412:'Precondition Failed',
-        413:'Request Entity Too Large',
-        414:'Request-URI Too Long',
-        415:'Unsupported Media Type',
-        416:'Request Range Not Satisfiable',
-        417:'Expectation Failed',
-        422:'Unprocessable Entity',
-        423:'Locked',
-        424:'Failed Dependency',
-        500:'Internal Server Error',
-        501:'Not Implemented',
-        502:'Bad Gatewate',
-        503:'Service Unavailable',
-        504:'Gateway Timeout',
-        505:'HTTP Version Not Supported',
-        507:'Insufficient Storage'
+    error_status_codes = {
+        400:{'message':'Bad Request'},
+        401:{'message':'Unauthorized'},
+        402:{'message':'Payment Required'},
+        403:{'message':'Forbidden'},
+        404:{'message':'Not Found'},
+        405:{'message':'Method Not Allowed'},
+        406:{'message':'Not Acceptable'},
+        407:{'message':'Proxy Authentication Required'},
+        408:{'message':'Request Timeout'},
+        409:{'message':'Conflict'},
+        410:{'message':'Gone'},
+        411:{'message':'Length Required'},
+        412:{'message':'Precondition Failed'},
+        413:{'message':'Request Entity Too Large'},
+        414:{'message':'Request-URI Too Long'},
+        415:{'message':'Unsupported Media Type'},
+        416:{'message':'Request Range Not Satisfiable'},
+        417:{'message':'Expectation Failed'},
+        422:{'message':'Unprocessable Entity'},
+        423:{'message':'Locked'},
+        424:{'message':'Failed Dependency'},
+        500:{'message':'Internal Server Error'},
+        501:{'message':'Not Implemented'},
+        502:{'message':'Bad Gateway'},
+        503:{'message':'Service Unavailable'},
+        504:{'message':'Gateway Timeout'},
+        505:{'message':'HTTP Version Not Supported'},
+        507:{'message':'Insufficient Storage'}
         }
-                     
-    # SPM - This may need to be an __init__
-    def __call__(self, status, message=None, sub_code=None, body_template=None):
-        if status not in allowed_codes:
-            raise ValueError, 'status code not permitted'
-        
-        self.status = status
-        self.title = status_codes.get('status')
-        self.explanation = message
-        self.sub_code = code
-        self.body_template = body_template
-
-    def prepare(self, environ):
-        """
-        Prepares the response for being called as a WSGI application \
-        (see pyramid.interfaces)
-        """
-        #TODO:
-        #self.app_iter = [???]
-        #self.body = ???
-        pass
+    
+    def __init__(self, exc, req):
+        self.exc = exc
+        self.req = req
+        super(RestError, self).__init__()
+    
+    # TODO: pass in environ and start_request as in super()__call__()? and use prepare?
+    def __call__(self):
+        if self.req.exception.code not in self.error_status_codes:
+            # TODO: make sure we have adequate handling? or allow a 500?
+            #super(RestError,self).__call__(self.req.environ, self.req???)
+            raise self.exc
+        response_dict = {
+            'code':self.exc.code,
+            'message':self.error_status_codes[self.exc.code]['message']
+        }
+        response = Response(dumps(response_dict))
+        response.status_int = self.exc.code
+        return response
