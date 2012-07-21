4b4a64e3d4d63ceae1189f84d93a468f6a983b59
Brian Neumann <brian@wapolabs.com>
2012-07-21 01:02:55 +0000

    Subclassing Resource to provide a Collection and Element.

--- wapo/pyramid/resources.py
+++ wapo/pyramid/resources.py
@@ -3,7 +3,6 @@
 class Resource(object):
     __name__ = None
     __parent__ = None
-    __children__ = None
 
     def __init__(self, parent=None, name=None):
         self.__name__ = name
@@ -17,3 +16,31 @@ class Resource(object):
         elif isinstance(self.__children__, object):
             return self.__children__(self, key)
         raise KeyError
+
+
+class Element(Resource):
+    """Represents an individual resource.
+
+    If the Element has any children, they should be defined in the children
+    dict, i.e.:
+    __children__ = {'child1name': Child1Class,
+                    'child2name': Child2Class}
+    """
+    __children__ = None
+
+    def __getitem__(self, key):
+        if isinstance(self.__children__, dict):
+            return self.__children__[key](self, key)
+        raise KeyError
+
+
+class Collection(Resource):
+    """Represents a collection of a single type of element.
+
+    The child class of the collection is represented as the
+    __child__ attribute.
+    """
+    __child__ = None
+
+    def __getitem__(self, key):
+        return self.__child__(self, key)
