f99184274d410639300436166c7cf729557da2f2
Daniel Li <daniel@wapolabs.com>
2012-09-28 17:55:10 -0700

    (1) Added JSONP support; (2) Cleaned up JSONError

--- wapo/pyramid/errors.py
+++ wapo/pyramid/errors.py
@@ -1,63 +1,54 @@
-from json import dumps
+"""Class to handle transformations of HTTP exceptions into JSON dictionaries.
+
+Copyright 2012 WaPo Labs
+
+[License]
+
+Author: Brian Neumann <brian@wapolabs.com>
+        Sean McBride <sean@wapolabs.com>
+        Daniel Li <daniel@wapolabs.com>
+
+This class transforms vanilla HTTP exceptions thrown by Pyramid into JSON
+dictionaries that include information on the exceptions, such as a status
+code and human-readable error message.  We created this class in order to
+keep all of Trove API's responses as JSON dictionaries, even in the case
+of HTTP exceptions.
+
+"""
+
 from pyramid.httpexceptions import HTTPError
-from pyramid.request import Response
+from pyramid.httpexceptions import HTTPRedirection
 
 
-class JSONError(HTTPError):
+class JSONError(HTTPError, HTTPRedirection):
+    """Class to transform vanilla Pyramid HTTP exceptions into JSON dicts.
+
+    This class transforms vanilla HTTP exceptions thrown by Pyramid into JSON
+    dictionaries that include information on the exceptions, such as a status
+    code and human-readable error message.  We created this class in order to
+    keep all of Trove API's responses as JSON dictionaries, even in the case
+    of HTTP exceptions.
+
+    For more information on how Pyramid organizes HTTP exceptions, see the
+    Pyramid narrative documentation:
+
+        http://docs.pylonsproject.org/projects/pyramid/en/latest/api/
+        httpexceptions.html
+
     """
-    Error Response
-    Implement with:
-        # TODO doublecheck import
-        from errors import JSONError
-        config.add_view(JSONError, context=HTTPError, renderer='json')
-    """
 
-    _error_status_codes = {
-        400: {'message': 'Bad Request'},
-        401: {'message': 'Unauthorized'},
-        402: {'message': 'Payment Required'},
-        403: {'message': 'Forbidden'},
-        404: {'message': 'Not Found'},
-        405: {'message': 'Method Not Allowed'},
-        406: {'message': 'Not Acceptable'},
-        407: {'message': 'Proxy Authentication Required'},
-        408: {'message': 'Request Timeout'},
-        409: {'message': 'Conflict'},
-        410: {'message': 'Gone'},
-        411: {'message': 'Length Required'},
-        412: {'message': 'Precondition Failed'},
-        413: {'message': 'Request Entity Too Large'},
-        414: {'message': 'Request-URI Too Long'},
-        415: {'message': 'Unsupported Media Type'},
-        416: {'message': 'Request Range Not Satisfiable'},
-        417: {'message': 'Expectation Failed'},
-        422: {'message': 'Unprocessable Entity'},
-        423: {'message': 'Locked'},
-        424: {'message': 'Failed Dependency'},
-        500: {'message': 'Internal Server Error'},
-        501: {'message': 'Not Implemented'},
-        502: {'message': 'Bad Gateway'},
-        503: {'message': 'Service Unavailable'},
-        504: {'message': 'Gateway Timeout'},
-        505: {'message': 'HTTP Version Not Supported'},
-        507: {'message': 'Insufficient Storage'}
-        }
+    def __init__(self, exception, request):
+        self._exception = exception
+        self._request = request
 
-    def __init__(self, exc, req):
-        self.exc = exc
-        self.req = req
         super(JSONError, self).__init__()
 
-    # TODO: pass in environ and start_request as in super()__call__()?
-    #       and use prepare?
     def __call__(self):
-        if self.req.exception.code not in self._error_status_codes:
-            # TODO: make sure we have adequate handling? or allow a 500?
-            #super(JSONError,self).__call__(self.req.environ, self.req???)
-            raise self.exc
+        self._request.response.status_int = self._exception.code
+
         response_dict = {
-            'code': self.exc.code,
-            'message': self.exc.detail or self.exc.explanation
-        }
-        self.req.response.status_int = self.exc.code
+            'code': self._exception.code,
+            'message': self._exception.detail or self._exception.explanation,
+            }
+
         return response_dict
---
+++ wapo/pyramid/renderers.py
@@ -0,0 +1,39 @@
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+from pyramid.renderers import JSONP as JSONPBase
+
+
+class JSONP(JSONPBase):
+    """Wrap jsonp responses with parameterized callback.
+    """
+
+    def __call__(self, info):
+        def _render(value, system):
+            request = system['request']
+            context = system['context']
+
+            if request.response.status_int == 200:
+                value['status'] = {'code': 200, 'message': 'OK'}
+            else:
+                # the error class renders the code/message here
+                value = {'status': value}
+
+            default = self._make_default(request)
+            val = self.serializer(value, default=default, **self.kw)
+            callback = request.GET.get(self.param_name)
+
+            if callback is None:
+                ct = 'application/json'
+                body = val
+            else:
+                ct = 'application/javascript'
+                body = '%s(%s)' % (callback, val)
+                request.response.status_int = 200
+            response = request.response
+            if response.content_type == response.default_content_type:
+                response.content_type = ct
+            return body
+        return _render
